c8bcd16fde6218f82a85ca6512ce7aae8e882e31
travis_bickle1985 <lujon@yandex.ru>
2015-07-14 21:56:59 +0300

    newa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 /**
  * Created by coadmin on 07.07.2015.
  */
@@ -7,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
 //        GUI gui = new GUI();
 //        gui.buildGUI();
-        X0GUI guiXo = new X0GUI();
-        guiXo.buildGUI();
+        //X0GUI guiXo = new X0GUI();
+        //guiXo.buildGUI();
+
+        biggerGUI bigger = new biggerGUI();
+        bigger.buildGUI();
 //        CalcGUI guiCalc = new CalcGUI();
 //        guiCalc.buildGUI();
     }
--- src/StepListener.java
+++ src/StepListener.java
@@ -5,18 +5,28 @@ import java.awt.event.ActionListener;
  * Created by coadmin on 10.07.2015.
  */
 public class StepListener implements ActionListener {
+
+    private X0GUI view; //чтобы предать все функции X0GUI
+
+    public StepListener (X0GUI view){ //чтобы предать все функции X0GUI
+        this.view = view;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         MyButton b = (MyButton) e.getSource();
 
         if (b.getText().equals(" ")) {
-            if ((X0GUI.state % 2) == 0) {
+            if ((view.getState() % 2) == 0) {
                 b.setText("O");
             } else {
                 b.setText("X");
             }
             //button{k].setEnabled(false);
-            X0GUI.state++;
+            view.setState();
+            view.changeTurnText();
+            view.hasWinner();
         }
+        
     }
 }
